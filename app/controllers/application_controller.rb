ce75e722d434f8896135495ca7d5aed9b1d66254
Mari Taksa <welch.mari@gmail.com>
2021-08-25 08:51:24 -0500

    tuesday afternoon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,12 +12,16 @@ class ApplicationController < Sinatra::Base
     money.to_json
   end
 
+  
+
   get "/foods/recipes" do
     resp = RestClient.get("https://api.spoonacular.com/recipes/findByIngredients?apiKey=6b11e8db33c64926a9e1523a34eb38e5&ingredients=salmon&number=10")
     resp.force_encoding('UTF-8')
     moolah = {data: resp}
     moolah.to_json
   end
+
+
 
 
 end
