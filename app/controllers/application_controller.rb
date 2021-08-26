9f3e5eb262565e74a1b65f5a09e3fb8d9bd4e4ef
Mari Taksa <welch.mari@gmail.com>
2021-08-26 09:01:31 -0500

    thurs morning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,17 +7,32 @@ class ApplicationController < Sinatra::Base
   # set :default_content_type, 'application/json'
 
   get "/foods" do
-    @foods = Food.all
-    @foods.to_json
+    foods = Food.all
+    foods.to_json
   end
-
-  
 
   get "/foods/recipes" do
     resp = RestClient.get("https://api.spoonacular.com/recipes/findByIngredients?apiKey=6b11e8db33c64926a9e1523a34eb38e5&ingredients=salmon&number=10")
     resp.force_encoding('UTF-8')
     moolah = {data: resp}
     moolah.to_json
+  end
+
+  get "/foods/:id" do
+    food = Food.find(params[:id])
+    food[:name]
+  end
+
+  patch "/foods/:id" do
+    food = Food.find(params[:id])
+    food.update(
+      # quantity: params[:quantity],
+      # unit: params[:unit],
+      # days_until_expiration: params[:days_until_expiration],
+      # date_of_purchase: params[:date_of_purchase],
+      category: params[:category]
+    )
+    food.to_json
   end
 
 
